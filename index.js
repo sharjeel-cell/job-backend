652e3940d6ff62407bea8a4b2cbc8263c7864813
college-demo <sharjeelahmad666777@gmail.com>
2025-10-20 23:44:30 -1200

    cors s

--- index.js
+++ index.js
@@ -13,6 +13,40 @@ dotenv.config({});
 
 const app = express();
 
+
+// ✅ Allowed Frontend Origins
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://job-frontend-cyan.vercel.app",
+  "https://job-frontend-git-main-sharjeel-ahmads-projects-6886ba83.vercel.app"
+];
+
+
+// ✅ CORS Setup
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      // Allow requests with no origin (like mobile apps, Postman)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+
+       console.log("❌ Blocked by CORS:", origin);
+      return callback(new Error("CORS not allowed"));
+    },
+    credentials: true, // ✅ allow cookies & auth headers
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  })
+);
+// ✅ Handle Preflight requests
+app.options("*", cors());
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+app.use(cookieParser());
+
+
 let isConnected = false;
 async function connectToMongoDB() {
     try {
@@ -37,22 +71,21 @@ app.use((req,res,next)=>{
 })
 
 // ✅ Setup CORS
-const corsOptions = {
-  origin: [
-    'http://localhost:5173',
-    'https://job-frontend-cyan.vercel.app',
-    'https://job-frontend-git-main-sharjeel-ahmads-projects-6886ba83.vercel.app' // optional
-  ],
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  credentials: true,
-};
+// const corsOptions = {
+//   origin: [
+//     'http://localhost:5173',
+//     'https://job-frontend-cyan.vercel.app',
+//     'https://job-frontend-git-main-sharjeel-ahmads-projects-6886ba83.vercel.app' // optional
+//   ],
+//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//   credentials: true,
+// };
 
-app.use(cors(corsOptions));
-app.options('*', cors(corsOptions)); // ✅ handle preflight
+// app.use(cors(corsOptions));
+// app.options('*', cors(corsOptions)); // ✅ handle preflight
 
-app.use(express.json());
-app.use(express.urlencoded({extended:true}))
-app.use(cookieParser());
+
+
 // const corsoption = {
 //     origin :[
 //       'http://localhost:5173',
@@ -64,7 +97,7 @@ app.use(cookieParser());
 // app.use(cors(corsoption));
 
 
-const PORT = process.env.PORT || 3000
+// const PORT = process.env.PORT || 3000
  // api's
  app.use('/api/v1/user',userRoute)
  app.use('/api/v1/company',companyRoute)
